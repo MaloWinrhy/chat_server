f1f05c09500df1baa245a19f342cf727d7355904
HenryMalo <winrhy@gmail.com>
2024-08-16 21:58:43 +0200

    feat: :sparkles: init listener and create Channel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream}; 
+use tokio::sync::broadcast;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader}; 
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    //init TCP listener
+	let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    //create channel
+    let (tx, _rx) = broadcast::channel::<String>(10);
+	Ok(())
 }
