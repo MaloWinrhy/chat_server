adfd327c3f32ce4cc8c4e248be4e202bfd66d4f2
HenryMalo <winrhy@gmail.com>
2024-08-16 22:26:39 +0200

    feat: :sparkles: add error and break

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,59 @@ use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    //init TCP listener
+    // Initialisation du listener TCP sur l'adresse et le port spécifiés
 	let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    //create channel
+    // Création d'un canal de diffusion pour permettre la communication entre les clients
     let (tx, _rx) = broadcast::channel::<String>(10);
 
-    loop{
+    loop {
+        // Acceptation des connexions entrantes
         let (mut socket, _) = listener.accept().await?;
-        //println!("test loop")
 
+        // Clonage du canal pour permettre la communication avec ce client spécifique
         let tx = tx.clone();
-
         let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+
+           
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+
+            loop {
+                tokio::select! {
+              
+                    result = reader.read_line(&mut line) => {
+                        match result {
+                            Ok(0) => break,
+                            Ok(_) => {
+                                if tx.send(line.clone()).is_err() {
+                                    break; 
+                                }
+                                line.clear(); 
+                            },
+                            Err(_) => break, 
+                        }
+                    }
+
+                    result = rx.recv() => {
+                        match result {
+                            Ok(msg) => {
+                                if writer.write_all(msg.as_bytes()).await.is_err() {
+                                    break; 
+                                }
+                            },
+                            Err(broadcast::error::RecvError::Lagged(_)) => {
+                                continue; 
+                            },
+                            Err(_) => break,
+                        }
+                    }
+                }
+            }
+        });
     }
-	Ok(())
 }
